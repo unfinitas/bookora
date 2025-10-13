0cc2cc72c89ea2432ba71efd51deff1786de4da3
Chambit Oh <chambit.oh@gmail.com>
2025-10-13 13:45:25 +0300

    feat: Set up Spring Boot project structure for Bookora application

---
+++ src/main/java/fi/unfinitas/bookora/BookoraApplication.java
@@ -0,0 +1,13 @@
+package fi.unfinitas.bookora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookoraApplication.class, args);
+	}
+
+}
